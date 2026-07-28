0177f5abe7d84062452050e6b2e191d6a40a2d3c
sharmithashalu <sharmithashalu@gmail.com>
2026-07-28 17:42:52 +0530

    solution

--- basic/upper_case.py
+++ basic/upper_case.py
@@ -14,8 +14,17 @@ print(x.swapcase())'''
 '''x = input('enter a name : ')
 print(x)'''
 
-x = int(input())
+'''x = int(input())
 if x %  2 == 0:
     print("even  number")
 else:
-    print("odd number")
+    print("odd number")'''
+
+
+x = input("enter a name :")
+for ch in x:
+    print(ch)
+    if "A" <= ch <= "Z":
+     print("upper case")
+    elif "a" <= ch <= "z":
+       print("lower case")
