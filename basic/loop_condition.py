b134a683b5c712ca23efbbe8dbce173e454102a5
sharmithashalu <sharmithashalu@gmail.com>
2026-07-29 18:09:46 +0530

    my multyply solution

--- basic/loop_condition.py
+++ basic/loop_condition.py
@@ -2,7 +2,11 @@
 for i in numbers:
     print(numbers)'''
 
+'''numbers = [10, 39, 56, 78, 90]
+for i in numbers:
+    print(i)'''
+
 numbers = [10, 39, 56, 78, 90]
 
 for i in numbers:
-    print(i)
+    print(i * 2)
