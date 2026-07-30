b11e19684f60748cbf8d25aa1a818ef2bdbddc98
sharmithashalu <sharmithashalu@gmail.com>
2026-07-30 17:37:38 +0530

    my total solution

--- basic/new.py
+++ basic/new.py
@@ -9,8 +9,17 @@ else:
     print("c is greater than a and b")'''
 
 
-x = int(input("enter a number : "))
+'''x = int(input("enter a number : "))
 if x % 2 == 0:
     print("even number")
 else:
-    print("odd number")
+    print("odd number")'''
+
+n = int(input("Enter a number: "))
+
+total = 0
+
+for i in range(1, n + 1):
+    total += i
+
+print("Sum =", total)
