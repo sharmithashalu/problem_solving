2e69537682d55002dd5595e3533586703cffe30e
sharmithashalu <sharmithashalu@gmail.com>
2026-08-03 20:56:09 +0530

    my second solution digit

--- basic/digit_sum.py
+++ basic/digit_sum.py
@@ -1,7 +1,15 @@
-n = int(input("enter a number :"))
+'''n = int(input("enter a number :"))
 sum = 0 
 while n > 0 :
     x = n % 10
     sum = sum + x
     n = n // 10
+print(sum)'''
+
+x = int(input())
+sum = 0
+while x > 0 :
+    y = x % 10
+    sum = sum + y
+    x = x // 10
 print(sum)
