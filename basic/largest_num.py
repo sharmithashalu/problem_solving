c5154987292975604eaaff4d85ed2d70073c8b62
sharmithashalu <sharmithashalu@gmail.com>
2026-08-04 21:13:52 +0530

    my smallest number

--- basic/largest_num.py
+++ basic/largest_num.py
@@ -1,4 +1,4 @@
-x = int(input("enter a number :"))
+'''x = int(input("enter a number :"))
 largest = 0
 
 while x > 0:
@@ -9,4 +9,16 @@ while x > 0:
 
     x = x // 10
 
-print(largest)
+print(largest)'''
+x = int(input("enter a number :"))
+smallest = x % 10
+
+while x > 0:
+    num = x % 10
+
+    if num < smallest:
+        smallest = num
+
+    x = x // 10
+
+print(smallest)
