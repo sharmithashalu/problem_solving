1d104e0771e7154c83802f9e71387cb879f2ab4c
sharmithashalu <sharmithashalu@gmail.com>
2026-08-06 07:18:54 +0530

    reverse number solution

--- basic/count_odd_digit.py
+++ basic/count_odd_digit.py
@@ -11,12 +11,17 @@ while x > 0 :
 print("even count = ", even)
 print("odd count = ", odd)'''
 
-x = int(input("Enter a number: "))
-
+'''x = int(input("Enter a number: "))
 count = 0
-
 while x > 0:
     count += 1
     x = x // 10
-   
-print("Digits =", count)
+print("Digits =", count)'''
+
+x = int(input("Enter a number: "))
+reverse = 0
+while x > 0:
+    digit = x % 10
+    reverse = reverse * 10 + digit
+    x = x // 10 
+print("Reverse =", reverse)
