0112a5e74d6982a2446fab801e3b53e65a92cd6f
sharmithashalu <sharmithashalu@gmail.com>
2026-08-07 21:14:09 +0530

    my reverse solution

--- basic/count_digit.py
+++ basic/count_digit.py
@@ -1,7 +1,16 @@
-x = int(input("enter a number :"))
+'''x = int(input("enter a number :"))
 count = 0
 while x > 0:
     x = x // 10
     count += 1
-print("count of digits :", count)
+print("count of digits :", count)'''
+
+x = int(input("enter a number :"))
+reverse = 0
+while x > 0 :
+    digit = x % 10
+    reverse = reverse * 10 + digit
+    x = x // 10
+print("reverse of number :",reverse)
+
   
