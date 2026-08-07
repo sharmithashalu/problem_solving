f450cbcdd259fab5ded57ba9616fcec186307ba3
sharmithashalu <sharmithashalu@gmail.com>
2026-08-07 21:20:48 +0530

    my event digit

--- basic/count_digit.py
+++ basic/count_digit.py
@@ -5,12 +5,21 @@ while x > 0:
     count += 1
 print("count of digits :", count)'''
 
-x = int(input("enter a number :"))
+'''x = int(input("enter a number :"))
 reverse = 0
 while x > 0 :
     digit = x % 10
     reverse = reverse * 10 + digit
     x = x // 10
-print("reverse of number :",reverse)
+print("reverse of number :",reverse)'''
+
+x = int(input("enter a number :"))
+even = 0
+while x > 0 :
+    digit = x % 10
+    if digit % 2 == 0 :
+        even += 1
+    x = x // 10
+print(" even digits :", even)
 
   
